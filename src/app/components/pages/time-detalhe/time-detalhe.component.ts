21e39b1066eb0b90ce0df11e56927e8c55b937ba
Moisés Batista <moises.desenv@gmail.com>
2024-06-12 11:22:52 -0300

    thales_chatao

--- src/app/components/pages/time-detalhe/time-detalhe.component.ts
+++ src/app/components/pages/time-detalhe/time-detalhe.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { Time } from 'src/app/interfaces/time';
 import { TimesService } from 'src/app/services/times.service';
 import { Location } from '@angular/common';
+import { ResultadoService } from 'src/app/services/resultado.service';
+import { Confronto } from 'src/app/interfaces/confronto';
 
 @Component({
   selector: 'app-time-detalhe',
@@ -12,16 +14,21 @@ import { Location } from '@angular/common';
 export class TimeDetalheComponent {
   idRecebido!: number;
   timeDetalhe!: Time;
+  confrontosPassados!: Confronto[];
+  confrontosFuturos!: Confronto[];
 
   constructor(
     private router: Router,
     private timeService: TimesService,
+    private resultadoService: ResultadoService,
     private location: Location
   ) {}
 
   ngOnInit(): void {
     this.idRecebido = this.getId();
     this.getTimeDetalhe();
+    this.getConfrontosPassados();
+    this.getConfrontosFuturos();
   }
 
   getId(): number {
@@ -32,7 +39,29 @@ export class TimeDetalheComponent {
     this.timeService.getTimeDetalhe(this.idRecebido).subscribe({
       next: (res: Time) => {
         this.timeDetalhe = res;
-        console.log(this.timeDetalhe);
+        console.log(this.timeDetalhe)
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  getConfrontosPassados() {
+    this.resultadoService.getConfrontosPassados(this.idRecebido).subscribe({
+      next: (res: Confronto[]) => {
+        this.confrontosPassados = res;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  getConfrontosFuturos() {
+    this.resultadoService.getConfrontosFuturos(this.idRecebido).subscribe({
+      next: (res: Confronto[]) => {
+        this.confrontosFuturos = res;
       },
       error: (error) => {
         console.log(error);
